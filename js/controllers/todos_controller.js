f315931206caf279cb2d47e43be1d9c79ec15718
morow93 <derchepur@gmail.com>
2014-05-12 11:33:30 +0400

    All Completed

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -16,9 +16,21 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
+    },
+    clearCompleted: function() {
+        var completed = this.filterBy('isCompleted', true);
+        completed.invoke('deleteRecord');
+        completed.invoke('save');
     }
   },
-    
+    hasCompleted: function() {
+      return this.get('completed') > 0;
+    }.property('completed'),
+
+    completed: function() {
+      return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+                                                     
     remaining: function() {
         return this.filterBy('isCompleted', false).get('length');
     }.property('@each.isCompleted'),
@@ -26,6 +38,16 @@ Todos.TodosController = Ember.ArrayController.extend({
     inflection: function() {
         var remaining = this.get('remaining');
         return remaining === 1 ? 'todo' : 'todos';
-    }.property('remaining')
+    }.property('remaining'),
+    
+    allAreDone: function(key, value) {
+      if (value === undefined) {
+        return !!this.get('length') && this.everyProperty('isCompleted', true);
+      } else {
+        this.setEach('isCompleted', value);
+        this.invoke('save');
+        return value;
+      }
+    }.property('@each.isCompleted')
                                                      
 });
